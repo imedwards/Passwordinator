5d18ce4cbedacf149e2bb9c3c3f7f81e92d0de7b
IMANI EDWARDS <imedwards720@gmail.com>
2024-01-08 22:59:54 -0800

    Added password parameters

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+  var number = "01234567890"
+  var symbol = "!@#$%^&*-_=+,<.>/?"
+
+
+
+
+
 
   passwordText.value = password;
 
